9f2ad2d9039aa38fcde3f7fa405d812ec5111b2a
Iris <agboolaisholaidreez>
2022-08-07 07:32:29 -0700

    update

--- src/middleware/validations/auth.ts
+++ src/middleware/validations/auth.ts
@@ -3,19 +3,16 @@ import { isEmail } from '../../utils/email';
 
 export const registerValidation = (req: Request, res: Response, next: NextFunction) => {
     try {
-        //return res.status(400).json({ msg: 'err' });
         const { firstname, lastname, email, password } = req.body;
         const error: any = {};
         if (!firstname || firstname?.trim() === '') error.firstname = 'firstname is required';
-
         if (!lastname || lastname?.trim() === '') error.lastname = 'lastname is required';
         if (!email || email?.trim() === '') error.email = 'email is required';
         if (!password || password?.trim() === '') error.password = 'password is required';
-
         if (Object.keys(error).length > 0) return res.status(400).json(error);
+
         if (!isEmail(email) || email !== email?.toLowerCase()) error.email = 'invalid email address';
         if (password.length < 6) error.password = 'password most be at least 6 character long';
-
         if (Object.keys(error).length > 0) return res.status(400).json(error);
 
         next();
